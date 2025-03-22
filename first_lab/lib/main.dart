012ace4f9fcc3877eadf4b57934f50618d81b34a
tOtiana23 <busygina_0404@mail.ru>
2025-03-22 13:48:09 +0900

    third lab

--- first_lab/lib/main.dart
+++ first_lab/lib/main.dart
@@ -1,62 +1,26 @@
+import 'package:first_lab/screens/second_screen.dart';
 import 'package:flutter/material.dart';
-import 'dart:math';
+
+import 'screens/first_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MainApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  final List<IconData> icons = [ //определяем какие иконки могут быть
-    Icons.access_alarm,
-    Icons.accessibility_new,
-    Icons.ac_unit,
-    Icons.account_circle,
-    Icons.ad_units,
-    Icons.airplanemode_active,
-    Icons.all_inbox,
-    Icons.android,
-    Icons.apartment,
-    Icons.archive,
-  ];
-
-  ///Выбираем рандомно иконку
-  IconData getRandomIcon() {
-    final random = Random();
-    return icons[random.nextInt(icons.length)];
-  }
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Бусыгина Татьяна'),
-        ),
-        body: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: List.generate(3, (_) => Icon(getRandomIcon())),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: List.generate(3, (_) => Icon(getRandomIcon())),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: List.generate(3, (_) => Icon(getRandomIcon())),
-            ),
-          ],
-        ),
-      ),
+      title: 'Бусыгина Татьяна. Калькулятор простых процентов',
+      home: FirstScreen(),
+      routes: {
+        '/second' : (context) => const SecondScreen(),
+      }
     );
   }
 }
+
+
